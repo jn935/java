82516f479c10b8c74f334cf111f7153c9d8cc191
Nikhil Jadhav <84830137+jn935@users.noreply.github.com>
2021-08-17 21:51:58 +0530

    Create AdditionOfTwoCharectors.java
    Write a program to adddition of two numbers also addition of two characters.

---
+++ AdditionOfTwoCharectors.java
@@ -0,0 +1,17 @@
+package day1;
+import java.util.Scanner;
+public class Test{
+	public static void main(String[] args) {
+		String no1;
+		String no2;
+		char result;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Charector =   ");
+		no1 = sc.next();
+		System.out.println("Enter Second Charector =  ");
+		no2 = sc.next();
+		
+		System.out.println(""+no1+""+no2);
+		
+	}
+}
