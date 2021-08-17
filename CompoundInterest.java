a27baf376b4acc0576eb65eda8b0fdac3ef7c0b9
Nikhil Jadhav <84830137+jn935@users.noreply.github.com>
2021-08-17 21:54:14 +0530

    Create CompoundInterest.java
    Find the compound amount and compound interest on the principal Rs.20,000 borrowed at 6% compounded annually for 3 years.

---
+++ CompoundInterest.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class Calculate {
+	public void calculate(int p,int t,double r,int n) {
+		double amount = p * Math.pow(1+(r/n), n*t);
+		double cinterest = amount - p;
+		System.out.println("Compound Interest after"+t+"years:"+cinterest);
+		System.out.println("Amount after"+t+"years:"+amount);
+}
+}
+package day1;
+
+public class TestCalculate {
+
+	public static void main(String[] args) {
+		Calculate c1 = new Calculate();
+		c1.calculate(20000,3,0.06,1);
+
+	}
+
+}
